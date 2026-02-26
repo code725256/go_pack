51ef58dd745f43fa42ba5bb31b5b0cfb02c0f6a0
code725256 <130452698+code725256@users.noreply.github.com>
2026-02-26 12:07:13 +0500

    go_test

---
+++ stringutils.go
@@ -0,0 +1,16 @@
+package stringutils
+
+func Uzindik(s string) int {
+	return len([]rune(s))
+}
+
+func Reverse(s string) string {
+	b := []byte(s)
+
+	for i := 0; i < len(b)/2; i++ {
+		j := len(b) - 1 - i
+		b[i], b[j] = b[j], b[i]
+	}
+
+	return string(b)
+}
